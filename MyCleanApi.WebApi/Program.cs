641db8cacabefecfc1f27267898d247d6cdd3b4d
danil <nnndk@yandex.ru>
2024-11-30 00:42:10 +0500

    delete bin & obj dirs

--- MyCleanApi.WebApi/Program.cs
+++ MyCleanApi.WebApi/Program.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpOverrides;
 using MyCleanApi.Application.Services;
 using MyCleanApi.Core.Interfaces;
 using MyCleanApi.Infrastructure;
 using MyCleanApi.Infrastructure.Repositories;
+using MyCleanApi.WebApi.Extensions;
 
 internal class Program
 {
@@ -10,14 +12,17 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        // DI
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("Docker")));
         builder.Services.AddScoped<IProductRepository, ProductRepository>();
         builder.Services.AddScoped<IProductService>();
 
+        // Add services to the container.
+        builder.Services.ConfigureCors();
+        builder.Services.ConfigureIISIntegration();
         builder.Services.AddControllers();
 
-        // Add services to the container.
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -39,17 +44,22 @@ internal class Program
             }
         }
 
-        /*if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        else
+            app.UseHsts();
+
+        app.UseStaticFiles();
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }*/
+            ForwardedHeaders = ForwardedHeaders.All
+        });
+        app.UseCors("CorsPolicy");
 
         app.UseSwagger();
         app.UseSwaggerUI();
 
         app.MapControllers();
-        app.MapGet("/", () => "Hello World!");
 
         app.Run();
     }
